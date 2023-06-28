716d8a4b59ff543adaf133f088ead5ff7007e1cb
pigflyflyfly <li.qinwen@qq.com>
2023-06-28 09:36:48 +0800

    first commit

---
+++ standardout.go
@@ -0,0 +1,11 @@
+package tools
+
+import (
+	"fmt"
+)
+
+func Printfln(format string, a ...interface{}) {
+
+	fmt.Printf(format+"\n", a...)
+
+}
---
+++ standardout_test.go
@@ -0,0 +1,7 @@
+package tools_test
+
+import "testing"
+
+func Printfln(t *testing.T) {
+
+}
